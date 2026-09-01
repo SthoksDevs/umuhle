923f068f409f9e53d7bf7068e39ef571350fd26a
TC NHLANGULELA <45557105+SthoksDevs@users.noreply.github.com>
2026-09-01 21:38:25 +0200

    fix: remove non-existent WelcomeEmailTemplate import causing build failure

--- emails/index.ts
+++ emails/index.ts
@@ -2,11 +2,6 @@
  * Welcome email index - exports all email functionality
  */
 
-export {
-  generateWelcomeEmail,
-  getEmailSubject,
-} from './components/WelcomeEmailTemplate';
-
 export { sendWelcomeEmail } from './services/emailService';
 
 export type { UserRole, EmailTemplateVariables, WelcomeEmailContent } from './types/welcomeEmailTypes';
